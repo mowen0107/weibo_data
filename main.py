f8a2416580f8d884e34c32d4694839537f54febd
黄子廷 <hzt@huangzitingdeMacBook-Pro.local>
2018-01-13 16:30:56 +0800

    add main 函数

---
+++ main.py
@@ -0,0 +1,14 @@
+''' Create time: 20180113
+    Author: HZT
+    Last modified time: 20180113
+    Last modified person: HZT
+    Last modified thing: 
+'''
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
