186b0b5d1b1331ab825df7f21b4f901ec8799bc1
hzt <huangziting0107@126.com>
2018-01-14 21:00:04 +0800

    添加清洗测试集的函数

--- washing.py
+++ washing.py
@@ -11,50 +11,68 @@ from numpy import nan as NA
 
 
 class Washing():
-    ''' 对训练集进行清洗
+    ''' 对训练集和测试集进行清洗
         校正或者删除错误的行
     '''
 
     def __init__(self):
         self.originPath = "C:/Users/yl/Desktop/data_mining/Task3/weibo_data/pro3_data/"
         self.outputPath = "C:/Users/yl/Desktop/data_mining/Task3/weibo_data/temp/"
-        self.fileName = "weibo_train_data2.txt"
+        self.trainDataFile = "weibo_train_data2.txt"
+        self.testDataFile = "weibo_predict_data.txt"
 
-    def run(self):
-        ''' 进行清洗的执行函数
+    def washTrainData(self):
+        ''' 进行训练集的清洗
         '''
-        originTrainData = self.readFile()
+        readFilePath = self.originPath + self.trainDataFile
+        writeFilePath = self.outputPath + self.trainDataFile
+        originTrainData = self.readFile(readFilePath)
         washedData = []
         for line in originTrainData:
             washedLine = self.replaceTable(line)
             washedData.append(washedLine)
-        self.writeFile(washedData)
+        self.writeFile(writeFilePath, washedData)
+        self.deleteWrongLines()
         print("------DONE------")
+
+    def washTestData(self):
+        ''' 进行测试集的清洗
+        '''
+        readFilePath = self.originPath + self.testDataFile
+        writeFilePath = self.outputPath+self.testDataFile
+        originTestData = self.readFile(readFilePath)
+        washedData = []
+        for line in originTestData:
+            washedLine = self.replaceTable(line)
+            washedData.append(washedLine)
+        self.writeFile(writeFilePath, washedData)
+        print("------DONE------")
+        pass
 
     def deleteWrongLines(self):
         ''' 删除无效的行
         '''
         nameList = ['luid', 'mid', 'time', 'fcs', 'ccs', 'lcs', 'cont']
         data = pd.read_csv(
-            self.outputPath + self.fileName, sep=',', names=nameList)
+            self.outputPath + self.trainDataFile, sep=',', names=nameList)
         for index in data.index:
-            print("------Index:",index,data.loc[index]['fcs'])
+            # print("------Index:",index,data.loc[index]['fcs'])
             fcs = data.loc[index]['fcs']
             ccs = data.loc[index]['ccs']
             lcs = data.loc[index]['lcs']
             if pd.isnull(fcs) and pd.isnull(ccs) and pd.isnull(lcs):
                 print("------NA:", index)
-                data.drop(index,axis=0,inplace=True)
-        data.to_csv(self.outputPath+"washedTrainData.txt",index=False,sep=",")
+                data.drop(index, axis=0, inplace=True)
+        data.to_csv(
+            self.outputPath + "washedTrainData.txt", index=False, sep=",")
 
-    def readFile(self):
+    def readFile(self, filePath):
         ''' 读文件操作，返回一个很大的string
         '''
         originTrainData = None
         originFile = None
         try:
-            originFile = open(
-                self.originPath + self.fileName, 'r', encoding='utf-8')
+            originFile = open(filePath, 'r', encoding='utf-8')
             originTrainData = originFile.readlines()
         except IOError as e:
             print("------ERROR LOG:", e)
@@ -64,13 +82,12 @@ class Washing():
                 originFile.close()
         return originTrainData
 
-    def writeFile(self, washedData):
+    def writeFile(self, filePath, washedData):
         ''' 写文件操作
         '''
         try:
             # w+,进行读写操作，文件不存在就创建，先清空再写
-            washedFile = open(
-                self.outputPath + self.fileName, 'w+', encoding='utf-8')
+            washedFile = open(filePath, 'w+', encoding='utf-8')
             washedFile.writelines(washedData)
         except IOError as e:
             print("------ERROR LOG:", e)
