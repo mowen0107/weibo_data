8df7f5a7fc4caa9c51daefc9d5e6e58551c9d7d9
黄子廷 <hzt@huangziingdeMBP.lan>
2018-01-14 19:10:52 +0800

    修改last modified date

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 ''' Author: HZT
     Create date: 20180113
     Last modified by: HZT
-    Last modified date: 20180113
+    Last modified date: 20180114
     Last modified thing: 
 '''
 from preprocessor import Preprocessor
--- washing.py
+++ washing.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ''' Author: HZT
-    Create date: 20180113
+    Create date: 20180114
     Last modified by: HZT
-    Last modified date: 20180113
+    Last modified date: 20180114
     Last modified thing: 
 '''
 
