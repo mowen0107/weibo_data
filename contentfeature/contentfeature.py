bfbd56ecda057f65268ba16c98099bb41fe71971
黄子廷 <hzt@huangzitingdeMacBook-Pro.local>
2018-01-19 21:22:18 +0800

    增加contentfeature.py及其测试文件

---
+++ contentfeature/contentfeature.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+''' Author: HZT
+    Create date: 20180119
+    Last modified by: HZT
+    Last modified date: 20180119
+    Last modified thing: 创建文件
+'''
+
+
+class ContentFeature():
+    def __init__(self):
+        pass
---
+++ contentfeature/test/test_contentfeature.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+''' Author: HZT
+    Create date: 20180119
+    Last modified by: HZT
+    Last modified date: 20180119
+    Last modified thing: 创建文件
+'''
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
